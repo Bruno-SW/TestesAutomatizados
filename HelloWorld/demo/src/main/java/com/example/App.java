9654f9d630a36c09b493951c5163adf7a7a987ff
= <brunosignoriw@gmail.com>
2022-09-30 14:33:52 -0300

    micro

--- HelloWorld/demo/src/main/java/com/example/App.java
+++ HelloWorld/demo/src/main/java/com/example/App.java
@@ -22,27 +22,13 @@ public class App {
         inputGoogle.clear();
         inputGoogle.sendKeys("Hello World!");
         inputGoogle.sendKeys(Keys.RETURN);
-
-        // // get the actual value of the title
-        // actualTitle = driver.getTitle();
-
-        // /*
-        //  * compare the actual title of the page with the expected one and print
-        //  * the result as "Passed" or "Failed"
-        //  */
-        // if (actualTitle.contentEquals(expectedTitle)){
-        //     System.out.println("Test Passed!");
-        // } else {
-        //     System.out.println("Test Failed");
-        // }
        
         try {
             Thread.sleep(5000);
         } catch (InterruptedException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
         }
-        //close Fire fox
+        
         driver.close();
        
     }
